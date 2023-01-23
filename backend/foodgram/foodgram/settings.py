c7b9c9195cecb2b7d4e276e9dfb786cdcc51da4d
Sokolnikov <sokolnikovbogdan1@gmail.com>
2023-01-24 00:07:56 +0300

    improing main.yml

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -98,17 +98,15 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
-
 AUTH_PASSWORD_VALIDATORS = [
+    {'NAME':
+     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME':
      'django.contrib.auth.password_validation.MinimumLengthValidator', },
     {'NAME':
      'django.contrib.auth.password_validation.CommonPasswordValidator', },
     {'NAME':
      'django.contrib.auth.password_validation.NumericPasswordValidator', },
-    {'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
-    }
 ]
 
 REST_FRAMEWORK = {
