5d156ed4344f3a495cbc038960d1f087e498f71d
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-02-16 13:40:45 +0300

    removed load_db.py

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -14,24 +14,19 @@ class Command(BaseCommand):
     help = 'loading ingredients from data in json'
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            'filename',
-            default='ingredients.json',
-            nargs='?',
-            type=str,
-        )
+        parser.add_argument('filename', default='ingredients.json', nargs='?',
+                            type=str)
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         try:
-            with open(os.path.join(DATA_ROOT, kwargs['filename']), 'r',
+            with open(os.path.join(DATA_ROOT, options['filename']), 'r',
                       encoding='utf-8') as f:
                 data = json.load(f)
                 for ingredient in data:
                     try:
-                        Ingredient.objects.create(
-                            name=ingredient["name"],
-                            measurement_unit=ingredient["measurement_unit"],
-                        )
+                        Ingredient.objects.create(name=ingredient["name"],
+                                                  measurement_unit=ingredient[
+                                                      "measurement_unit"])
                     except IntegrityError:
                         print(f'Ингридиет {ingredient["name"]} '
                               f'{ingredient["measurement_unit"]} '
