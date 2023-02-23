c1ad4f1a1207e922ade592533001b7495fbe408d
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-02-23 21:31:25 +0300

    couze of problems with static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -102,7 +102,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ORIGIN_ALLOW_ALL = True
